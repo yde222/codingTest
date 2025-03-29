7b19717c321db3597fe10e81339727f560927ec7
yde222 <150855134+yde222@users.noreply.github.com>
2025-03-30 01:24:53 +0900

    [Bronze V] Title: 사칙연산, Time: 92 ms, Memory: 12868 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner n = new Scanner(System.in);
+		int A = n.nextInt();
+		int B = n.nextInt();
+		
+		System.out.println(A + B);
+		System.out.println(A - B);
+		System.out.println(A * B);
+		System.out.println(A / B);
+		System.out.println(A % B);
+		
+	}
+}
