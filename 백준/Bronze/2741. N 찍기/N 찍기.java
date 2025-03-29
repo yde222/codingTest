2384d98527a0122edec19fdcd2c4bd0ce3426a4e
yde222 <150855134+yde222@users.noreply.github.com>
2025-03-30 01:18:32 +0900

    [Bronze V] Title: N 찍기, Time: 1096 ms, Memory: 34236 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner n = new Scanner(System.in);
+		int N = n.nextInt();
+		
+		for(int i=1;i<=N;i++) {
+			System.out.println(i);
+		}
+	}
+}
