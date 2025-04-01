8abb8e7aa5ce0b7eaa3358f2cd2598e4635a29a4
yde222 <150855134+yde222@users.noreply.github.com>
2025-04-01 12:35:02 +0900

    [Bronze V] Title: 고양이, Time: 64 ms, Memory: 11232 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		
+		System.out.println("\\    /\\"+"");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+		
+	}
+}
