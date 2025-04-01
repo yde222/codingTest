71e5b1de77bb6d7dcf31f1561c6411068becfc6b
yde222 <150855134+yde222@users.noreply.github.com>
2025-04-01 14:40:52 +0900

    [Bronze V] Title: 아스키 코드, Time: 92 ms, Memory: 12792 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		char ch = sc.nextLine().charAt(0);
+		int a = (int)ch;
+		
+		
+		System.out.println(a);
+		
+		
+	
+
+		
+	}
+}
