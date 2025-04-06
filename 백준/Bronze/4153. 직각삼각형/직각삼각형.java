cdc4c77e0d274643ccd6e19111715d7f3a1a7cdf
yde222 <150855134+yde222@users.noreply.github.com>
2025-04-06 16:56:18 +0900

    [Bronze III] Title: 직각삼각형, Time: 96 ms, Memory: 12904 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,29 +1,27 @@
 import java.util.Scanner;
 
-public class Main {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        while (true) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
-
-    
-            if (a == 0 && b == 0 && c == 0) break;
-
-     
-            int max = Math.max(a, Math.max(b, c));
-            int sum = a * a + b * b + c * c - max * max;
-
-            if (max * max == sum) {
-                System.out.println("right");
-            } else {
-                System.out.println("wrong");
-            }
-        }
-
-        sc.close();
-    }
+public class Main{
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			
+			if(a==0 && b==0 && c==0) break;
+			
+			int max = Math.max(a, Math.max(b, c));
+			int sum = a*a + b*b + c*c -max*max;
+		
+			if(max*max ==sum) {
+				System.out.println("right");
+			}else {
+				System.out.println("wrong");
+			}
+		}
+		
+		sc.close();
+	}
 }
