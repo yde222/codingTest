86eb16ff4ba79a2a60d42732617ac3d60db69078
yde222 <150855134+yde222@users.noreply.github.com>
2025-10-05 23:07:11 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 3.85 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,45 +1,38 @@
 import java.util.*;
 
-class Solution {
-    
-   
-    
+class Solution{
     public int[] solution(String today, String[] terms, String[] privacies) {
-        // 약관별 유효기간을 저장할 Map
         Map<String, Integer> termMap = new HashMap<>();
-        for (String term : terms) {
-            String[] parts = term.split(" ");
-            String termType = parts[0];
-            int period = Integer.parseInt(parts[1]);
-            termMap.put(termType, period);
+        for (String t : terms) {
+            String[] p = t.split(" ");
+            termMap.put(p[0], Integer.parseInt(p[1]));
         }
-        
-        // 오늘 날짜를 일수로 변환
-        int todayDays = dateToDay(today);
-        
-        // 파기해야 할 개인정보 번호를 저장할 리스트
-        List<Integer> result = new ArrayList<>();
-        
-        // 각 개인정보 검사
-        for (int i = 0; i < privacies.length; i++) {
-            String[] parts = privacies[i].split(" ");
-            String collectDate = parts[0];
-            String termType = parts[1];
-            
-            // 수집 날짜를 일수로 변환
-            int collectDays = dateToDay(collectDate);
-            
-            // 유효기간을 일수로 변환하여 만료일 계산
-            int validPeriodDays = termMap.get(termType) * 28;
-            int expireDays = collectDays + validPeriodDays;
-            
-            // 오늘 날짜가 만료일보다 크거나 같으면 파기 대상
-            if (todayDays >= expireDays) {
-                result.add(i + 1); // 1번부터 시작
+        int todayDays = toDays(today);
+        List<Integer> res = new ArrayList<>();
+
+        for(int i =0; i< privacies.length; i++ ){
+            String[] p = privacies[i].split(" ");
+            String date = p[0];
+            String termType = p[1];
+
+            int collectedDays = toDays(date);
+            int termMonths = termMap.get(termType);
+
+            int expireDays = collectedDays + termMonths *28 -1;
+
+            if(todayDays > expireDays){
+                res.add(i+1);
             }
         }
-        
-        // List를 배열로 변환하여 반환
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        return res.stream().mapToInt(i->i).toArray();
     }
+
+    private int toDays(String date){
+        String[] s = date.split("\\.");
+        int y = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+        int d = Integer.parseInt(s[2]);
+        return y *12 *28 + m*28 +d;
+    }
+
 }
