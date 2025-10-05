6b59430a0fef09f143981f93062cbe6e2221bc57
yde222 <150855134+yde222@users.noreply.github.com>
2025-10-05 12:31:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 101.72 ms, Memory: 98.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+     public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String name : participant){
+            map.put(name, map.getOrDefault(name, 0) + 1);
+        }
+
+        for(String name : completion){
+            map.put(name, map.get(name) - 1);
+        }
+
+        for(String name : map.keySet()){
+            if(map.get(name)==1){
+                return name;
+            }
+        }
+        return "";
+    }
+}
